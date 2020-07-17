0144df43fe52346734737eb2ff302266b67fa498
Bruno <brunomarcianosantos@gmail.com>
2020-07-17 14:12:46 -0300

    Atualização adicionando 2 novas funcionalidades

--- app/src/main/java/com/example/atividadeintent/ActivityResultado.java
+++ app/src/main/java/com/example/atividadeintent/ActivityResultado.java
@@ -17,15 +17,22 @@ public class ActivityResultado extends AppCompatActivity {
         TextView resultEmail = findViewById(R.id.txtresultEmail);
         TextView resultCpf = findViewById(R.id.txtresultCpf);
         TextView resultCell = findViewById(R.id.txtresultCell);
+        TextView resultSexo = findViewById(R.id.txtresultSexo);
+        TextView resultEnviaremail = findViewById(R.id.txtresultEnviaremail);
 
         String nome = extras.getString(MainActivity.EXTRA_MESSAGE_NOME);
         String email = extras.getString(MainActivity.EXTRA_MESSAGE_EMAIL);
         String cpf = extras.getString(MainActivity.EXTRA_MESSAGE_CPF);
         String cell = extras.getString(MainActivity.EXTRA_MESSAGE_CELL);
+        String sexo = extras.getString(MainActivity.EXTRA_MESSAGE_SEXO);
+        String dados = extras.getString(MainActivity.EXTRA_MESSAGE_ENVIAREMAIL);
+
 
         resultNome.setText(nome);
         resultEmail.setText(email);
         resultCpf.setText(cpf);
         resultCell.setText(cell);
+        resultSexo.setText(sexo);
+        resultEnviaremail.setText(dados);
     }
 }
--- app/src/main/java/com/example/atividadeintent/MainActivity.java
+++ app/src/main/java/com/example/atividadeintent/MainActivity.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE_NOME = "com.example.intent.NOME";
     public final static String EXTRA_MESSAGE_EMAIL = "com.example.intent.EMAIL";
     public final static String EXTRA_MESSAGE_CPF = "com.example.intent.CPF";
     public final static String EXTRA_MESSAGE_CELL = "com.example.intent.CELL";
+    public final static String EXTRA_MESSAGE_SEXO = "com.example.intent.SEXO";
+    public final static String EXTRA_MESSAGE_ENVIAREMAIL = "com.example.intent.ENVIAREMAIL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +29,36 @@ public class MainActivity extends AppCompatActivity {
         EditText txtEmail = findViewById(R.id.txtEmail);
         EditText txtCpf = findViewById(R.id.txtCpf);
         EditText txtCell = findViewById(R.id.txtCell);
+        CheckBox ckbSexo = findViewById(R.id.ckbSexo);
+        Switch swtEnviaremail = findViewById(R.id.swtEnviaremail);
 
         String nome = txtNome.getText().toString();
         String email = txtEmail.getText().toString();
         String cpf = txtCpf.getText().toString();
         String cell = txtCell.getText().toString();
+        String dados;
+        String sexo;
 
+        if (swtEnviaremail.isChecked()){
+            dados = "Dados enviados ao email cadastrado!";
+        }
+        else{
+            dados = "Cópia dos dados não enviada!";
+        }
 
+        if (ckbSexo.isChecked()){
+            sexo = "Sexo Masculino";
+        }
+        else{
+            sexo = "Sexo Feminino";
+        }
         Intent intent = new Intent(this, ActivityResultado.class);
         intent.putExtra(EXTRA_MESSAGE_NOME, nome);
         intent.putExtra(EXTRA_MESSAGE_EMAIL, email);
         intent.putExtra(EXTRA_MESSAGE_CPF, cpf);
         intent.putExtra(EXTRA_MESSAGE_CELL, cell);
+        intent.putExtra(EXTRA_MESSAGE_SEXO, sexo);
+        intent.putExtra(EXTRA_MESSAGE_ENVIAREMAIL, dados);
         startActivity(intent);
 
     }
